8af3e62f1eb213cd0a8e65e1cc64b358e2a7960e
Ezequiel <ezequiel.maranda@gmail.com>
2024-11-06 13:40:01 -0300

    simple select with grizzle created

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Hola soy una app de Next.js</h1>
+import 'dotenv/config';
+import { usersTable } from '../db/schema';
+import { db } from '../db/connection';
+  
+// Tipo para una fila de `usersTable`
+type User = typeof usersTable.$inferSelect;
+
+export default async function Home() {
+  const users: Array<User> = await db.select().from(usersTable);
+
+    return (
+      <main>
+      <h1>Hola, soy una app de Next.js</h1>
+      <ul>
+        {users.length > 0 ? (
+          users.map((user) => (
+            <li key={user.id}>
+              {user.name}
+            </li>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </ul>
     </main>
   );
 }
--- db/index.ts
+++
@@ -1,7 +0,0 @@
-import { drizzle } from 'drizzle-orm/neon-http';
-
-if (!process.env.DATABASE_URL) {
-    throw new Error("DATABASE_URL no está definida en las variables de entorno.");
-  }
-  
-const db = drizzle(process.env.DATABASE_URL);
