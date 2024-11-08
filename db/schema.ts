b04e22c39064c5566091875ce3fe00fb1e75bf3a
Ezequiel <ezequiel.maranda@gmail.com>
2024-11-08 11:57:35 -0300

    user schema updated

--- db/schema.ts
+++ db/schema.ts
@@ -1,8 +1,10 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, text, timestamp } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
+  passwordHash: text('password_hash').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
 });
