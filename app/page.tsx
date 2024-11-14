3e83f235767412db1c8324648378f1a1f67360bf
Ezequiel <ezequiel.maranda@gmail.com>
2024-11-13 23:17:49 -0300

    homepage updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,7 @@
-"use client";
-
-import LoginForm from "./loginForm";
-
 export default function Home() {
   return (
     <main className="flex flex-col m-10 justify-center items-center">
       <h1 className="my-4 text-3xl">Bienvenido!</h1>
-
-      <LoginForm />
-
-      <button
-        className="my-3 max-w-40 bg-violet-900 border border-violet-700 rounded-md px-2 hover:bg-violet-700 transition-all"
-        onClick={() => (window.location.href = "/admin")}
-      >
-        Admin page
-      </button>
     </main>
   );
 }
